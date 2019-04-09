6a52187937f7e1670c9e24905cefe97ca219c02b
vinita gupta <vinita.gupta@sapient.com>
2019-04-09 20:47:32 +0530

    added code

---
+++ index.js
@@ -0,0 +1,13 @@
+new Vue({
+	el: '#exercise',
+  data: {
+  	name: 'Vinita',
+    age: '27',
+    imagesrc: 'http://www.minionshop.co.uk/images/productBigZoom/DE012.jpg'
+  },
+  methods: {
+  	randomNum() {
+    	return Math.random();
+    }
+  }
+})
